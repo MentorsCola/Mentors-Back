88cc7a70413e0836ec7708bd6287fd8e55362476
shk0625 <tjfgml1238@gmail.com>
2023-11-06 19:45:40 +0900

    url 추가

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from user.views import RegisterAPIView, AuthAPIView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()), # post - 회원가입
     path("auth/", AuthAPIView.as_view()), # post - 로그인, delete - 로그아웃, get - 유저정보
+    path("auth/refresh/", TokenRefreshView.as_view()), # jwt 토큰 재발급
 
 ]
