3220c08959fb42941a33d55a9e8329988553b391
shk0625 <tjfgml1238@gmail.com>
2023-11-06 19:54:29 +0900

    ✅ auth view(test용) 생성 url

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from user.views import RegisterAPIView, AuthAPIView
+from user.views import RegisterAPIView, AuthAPIView, UserViewSet
+
+router = routers.DefaultRouter()
+router.register('list', UserViewSet) # 유저리스트 (테스트용)
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()), # post - 회원가입
     path("auth/", AuthAPIView.as_view()), # post - 로그인, delete - 로그아웃, get - 유저정보
     path("auth/refresh/", TokenRefreshView.as_view()), # jwt 토큰 재발급
-
+    path("", include(router.urls)),
 ]
