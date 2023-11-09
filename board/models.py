139096334dca94863faebab9df2c9ef42bbf0664
shk0625 <tjfgml1238@gmail.com>
2023-11-09 19:50:39 +0900

    ✨ board models 생성

---
+++ board/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from user.models import User
+
+
+class Board(models.Model):  # 제목, 작성자, 내용, 작성일, 마지막 수정일
+    title = models.CharField("제목", max_length=50, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField("내용", null=False)
+    dt_created = models.DateTimeField("작성일", auto_now_add=True, null=False)
+    dt_modified = models.DateTimeField("수정일", auto_now_add=True, null=False)
+
+    def __str__(self):
+        return 'author : {}, title : {}'.format(self.author, self.title, self.content)
+
