4507b335d8429e9f569dbafc5541b3bf78da3e02
shk0625 <tjfgml1238@gmail.com>
2023-11-15 17:08:35 +0900

    ✨Board(객체) view create

--- board/views.py
+++ board/views.py
@@ -1,1 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from board.models import Board
+from board.serializer import BoardSerializer, BoardDetailSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+class BoardAPI(generics.ListCreateAPIView): # 로그인하지 않아도 게시글 읽을 수 있음
+    ueryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class BoardDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardDetailSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsOwnerOrReadOnly]
