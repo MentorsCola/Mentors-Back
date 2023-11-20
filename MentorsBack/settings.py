0bd041d684caa86f57695e549fba6da18993ba69
shk0625 <tjfgml1238@gmail.com>
2023-11-20 18:26:02 +0900

    🔧

--- MentorsBack/settings.py
+++ MentorsBack/settings.py
@@ -161,13 +161,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_TZ = False
+
+APPEND_SLASH = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
