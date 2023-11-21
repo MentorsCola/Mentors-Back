4c3f5ce47de796b74aea75522c90e5058263ceaf
shk0625 <tjfgml1238@gmail.com>
2023-11-21 19:49:06 +0900

    💡

--- MentorsBack/urls.py
+++ MentorsBack/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/register/", RegisterAPIView.as_view()),  # post - 회원가입
     path("api/user/auth/", AuthAPIView.as_view()),  # post - 로그인, delete - 로그아웃, get - 유저정보
-    path("api/user/auth/refresh/", TokenRefreshView.as_view()),  # jwt 토큰 재발급
+    path("api/user/auth/refresh/", TokenRefreshView.as_view()),  # post jwt 토큰 재발급
     path("", include(router.urls)),
     path("", include("board.urls")),
 ]
