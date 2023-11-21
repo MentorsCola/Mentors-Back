800c5682f6743c0a70819e8e5f5b9ec919af53c5
shk0625 <tjfgml1238@gmail.com>
2023-11-21 20:36:54 +0900

    ♻️ url 전면 수정

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
+from user.views import RegisterAPIView, AuthAPIView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("board.urls")),
-    path("auth/", include("user.urls")),
+
+    path("api/user/register/", RegisterAPIView.as_view()),  # post - 회원가입
+    path("api/user/auth/", AuthAPIView.as_view()),  # post - 로그인, delete - 로그아웃, get - 유저정보
+    path("api/user/auth/refresh/", TokenRefreshView.as_view()),  # post jwt 토큰 재발급
+
 ]
