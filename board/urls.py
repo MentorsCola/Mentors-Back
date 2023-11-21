6776f3976cc049928425e25f8d1f8b937ff6a8e6
shk0625 <tjfgml1238@gmail.com>
2023-11-21 20:36:50 +0900

    ♻️ url 전면 수정

--- board/urls.py
+++ board/urls.py
@@ -5,5 +5,5 @@ app_name = "board"
 
 urlpatterns = [
     path("board/", views.BoardAPI.as_view(), name="board-list"),
-    path("board/<int:pk>/", views.BoardDetailAPI.as_view(), name="board-detail"),
+    path("board/<int:pk>/", views.BoardDetailAPI.as_view(), name="board-detail"), # 게시물 내용 보는건 로그인을 해야함
 ]
